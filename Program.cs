56c7f6e299f88b7f72361cbadc1caec2310317d7
Daniil Obod <DaniilO@neogames.com>
2024-07-07 21:00:08 +0300

    new

--- Program.cs
+++ Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Homework with Git, second commit.");
 Console.WriteLine("Homework with Git, one more commit.");
 Console.WriteLine("Homework with Git, and new code line.");
+Console.WriteLine("Homework with Git, and new code line.");
